c82d668c77e3f1d5b81af1c65c4e37c578127a55
exceldigital1 <saheed.adedapo111@gmail.com>
2026-02-17 08:54:38 +0100

    fixed other links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ function Footer() {
                 <a href="mailto:saheed.adedapo111@gmail.com"><i className="fa-solid fa-envelope"></i></a>
                 <a href="tel:+2349078740655"><i className="fa-solid fa-phone"></i></a>
                 <a href="https://x.com/saheeddamilare3?s=21"><i className="fa-brands fa-twitter"></i></a>
-                <a href="https://linkedin.com"><i className="fa-brands fa-linkedin"></i></a>
-                <a href="https://github.com"><i className="fa-brands fa-github"></i></a>
+                <a href="https://linkedin.com/in/saheed-adedapo-702052284"><i className="fa-brands fa-linkedin"></i></a>
+                <a href="https://github.com/damdevtech"><i className="fa-brands fa-github"></i></a>
             </div>
         </footer>
     );
