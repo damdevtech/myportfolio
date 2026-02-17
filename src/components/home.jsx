4f4aa22a068e8937b4394294167cb53cb92d546a
exceldigital1 <saheed.adedapo111@gmail.com>
2026-02-17 12:26:29 +0100

    i added calculator to projects

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,8 +7,8 @@ function Home() {
                 <h1>Hi, I'm Saheed, <br /> A frontend Developer</h1>
                 <p>I build accessible, inclusive digital products and web experiences, and I’m currently open to internship opportunities where I can learn, grow, and contribute.</p>
                 <div className="main-links">
-                    <a id="my-work" href="">View My Work</a>
-                    <a id="my-cv" href="">Download CV</a>
+                    <a id="my-work" href="#projects">View My Work</a>
+                    <a id="my-cv" href="#">Download CV</a>
                 </div>
             </div>
             <img src="https://bairesdev.mo.cloudinary.net/blog/2022/08/portrait-of-a-man-using-a-computer-in-a-modern-office-picture-id1344688156-1.jpg?tx=w_1920,q_auto" alt="Developer at work" />
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import ProjectCard from './ProjectCard.jsx'
-// import calculator from "../assets/calculator.png";
+import calculator from "../assets/calculator.png";
 import meal from "../assets/meal-generator.png";
 import quiz from "../assets/quiz.png";
 import cv from "../assets/cv-builder.png"; 
@@ -12,7 +12,7 @@ const projects = [
     { image: quiz, title: "Quiz App", demoLink: "https://damdevquizapp.netlify.app", githubLink: "https://github.com/damdevtech/quizapp", description: "An interactive quiz application built with React and Tailwind CSS.", skills: ["React", "Tailwind CSS", "JavaScript"] },
     {image: cv, title: "CV Builder App", demoLink: "https://damdevcvbuilder.netlify.app", githubLink: "https://github.com/damdevtech/CV-Builder", description: "An application for creating professional CVs with a clean UI.", skills: ["React", "Tailwind CSS", "JavaScript"] },
     { image: mini, title: "Mini Projects", demoLink: "https://15reactminiprojects.netlify.app", githubLink: "https://github.com/damdevtech/15-react-mini-projects ", description: "A collection of small, fun projects built with React and Tailwind CSS.", skills: ["React", "Tailwind CSS", "JavaScript"] },
-    // { image: calculator, title: "Calculator App", demoLink: "", githubLink: "", description: "A fully functional calculator with a modern design.", skills: ["HTML", "CSS", "JS"] },
+    { image: calculator, title: "Calculator App", demoLink: "https://damdevcalculator.netlify.app/", githubLink: "https://github.com/damdevtech/calculator", description: "A fully functional calculator with a modern design.", skills: ["HTML", "CSS", "JS"] },
     { image: meal, title: "Meal Generator", demoLink: "https://random-mealgenerator.netlify.app", githubLink: "https://github.com/damdevtech/random-meal-generator", description: "An app that generates random meal ideas for users.", skills: ["HTML", "CSS", "JS"] },
 ];
 
