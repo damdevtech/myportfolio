331c423f79e105c1aa9cbedd93e827958054471a
exceldigital1 <saheed.adedapo111@gmail.com>
2026-04-20 12:49:47 +0100

    i added my cv file

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import myCv from "../assets/Saheed_Damilare_Frontend_CV.pdf"
 
 function Home() {
     return (
@@ -8,7 +9,7 @@ function Home() {
                 <p>I build accessible, inclusive digital products and web experiences, and I’m currently open to internship opportunities where I can learn, grow, and contribute.</p>
                 <div className="main-links">
                     <a id="my-work" href="#projects">View My Work</a>
-                    <a id="my-cv" href="#">Download CV</a>
+                    <a id="my-cv" href={myCv} target="_blank" rel="noopener noreferrer">View CV</a>
                 </div>
             </div>
             <img src="https://bairesdev.mo.cloudinary.net/blog/2022/08/portrait-of-a-man-using-a-computer-in-a-modern-office-picture-id1344688156-1.jpg?tx=w_1920,q_auto" alt="Developer at work" />
